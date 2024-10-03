dbcd1a8542087b09f92ad0d7f9d8e1d1664063d1
pedreopj <pelaezescobarpepo@gmail.com>
2024-10-03 06:45:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-st.title("Tablero para dibujo")
+# Cambiamos el título a "dobuja kbro"
+st.title("dobuja kbro")
 
 with st.sidebar:
-  st.subheader("Propiedades del Tablero")
-  drawing_mode = st.sidebar.selectbox(
-    "Herramienta de Dibujo:",
-    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
-  )
-  
-  stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-  stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
-  bg_color = '#000000'
+    st.subheader("Propiedades del Tablero")
+    drawing_mode = st.sidebar.selectbox(
+        "Herramienta de Dibujo:",
+        ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+    )
 
-# Create a canvas component
+    stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+    stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
+    bg_color = '#000000'
+
+# Crear el componente del canvas
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    fill_color="rgba(255, 165, 0, 0.3)",  # Color de relleno con algo de opacidad
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
